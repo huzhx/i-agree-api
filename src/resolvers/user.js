6dc4c60254ee5f4c9874cb7255634f446a559eab
Zhaoxian Hu <huzhx@uci.edu>
2021-02-22 14:47:21 -0800

    add resolver to handle the updateBaselinePreference mutation and update the verification of the baselinePreferences completeness

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,7 +1,7 @@
 const resolvers = {
   Query: {
     baselinePreferencesCompleted: (parent, { userId }, { models }) => {
-      return userId in models.baselinePreference;
+      return userId in models.baselinePreference && Object.keys(models.baselinePreference[userId]).length === 8;
     },
     getBaselinePreference: (parent, { userId, institutionType }, { models }) => {
       if (userId in models.baselinePreference) {
@@ -9,6 +9,21 @@ const resolvers = {
       } else {
         return null;
       }
+    },
+  },
+
+  Mutation: {
+    updateBaselinePreference: (parent, { userId, institutionType, consentState }, { models }) => {
+      models.baselinePreference[userId] = models.baselinePreference[userId] || {};
+      if (typeof models.baselinePreference[userId]['userId'] === 'undefined') {
+        models.baselinePreference[userId]['userId'] = userId;
+      }
+      models.baselinePreference[userId][institutionType] = consentState;
+      console.log(models.baselinePreference[userId]);
+      return {
+        institutionType,
+        consentState,
+      };
     },
   },
 };
