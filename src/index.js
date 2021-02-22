65faf888886aee2b1c06a98d29be3886d0c97637
Zhaoxian Hu <huzhx@uci.edu>
2021-02-22 12:11:21 -0800

    run apollo server with schema, resolvers, and models in context

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+
+import schema from './schema';
+import resolvers from './resolvers';
+import models from './models';
 
 console.log('Hello running iAgree Project.');
 
 console.log(process.env.MY_SECRET);
+
+const app = express();
+
+app.use(cors());
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: {
+    models,
+  },
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8080 }, () => {
+  console.log('Apollo Server on http://localhost:8080/graphql');
+});
