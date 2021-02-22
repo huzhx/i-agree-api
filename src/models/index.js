9b6bd2015bbd3898bd13e42a50512cbfefb6eb81
Zhaoxian Hu <huzhx@uci.edu>
2021-02-22 12:08:53 -0800

    add baselinePreference sample data

---
+++ src/models/index.js
@@ -0,0 +1,14 @@
+const baselinePreference = {
+  1: {
+    userId: '1',
+    DOCTORSOFFICE: 93,
+    HOSPITAL: 77,
+    INSURANCE: 79,
+    STATEHA: 111,
+    GOVHA: 108,
+    BIOTECHCOMPANY: 44,
+    UNIVERSITY: 109,
+  },
+};
+
+export default { baselinePreference };
