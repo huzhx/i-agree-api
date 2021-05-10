095bb0f2846d670512f0604961537030afc6d2e9
Zhaoxian Hu <huzhx@uci.edu>
2021-05-10 14:48:20 -0700

    correct the type errors

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -2,43 +2,52 @@ import { ApolloError } from 'apollo-server-errors';
 
 const resolvers = {
   Query: {
-    baselinePreferencesCompleted: (parent, { userId }, { models }) => {
+    baselinePreferencesCompleted: (parent: any, { userId }: { userId: string }, { models }: { models: any }) => {
       return userId in models.baselinePreference && Object.keys(models.baselinePreference[userId]).length === 8;
     },
-    getBaselinePreference: (parent, { userId, institutionType }, { models }) => {
+    getBaselinePreference: (
+      parent: any,
+      { userId, institutionType }: { userId: string; institutionType: string },
+      { models }: { models: any }
+    ) => {
       if (userId in models.baselinePreference) {
         return models.baselinePreference[userId][institutionType];
       } else {
         return null;
       }
     },
-    getPendingStudiesNumber: (parent, args, { models, isAuth, userId }) => {
+    getPendingStudiesNumber: (
+      parent: any,
+      args: any,
+      { models, isAuth, userId }: { models: any; isAuth: boolean; userId: string }
+    ) => {
       if (!isAuth) {
         throw new ApolloError('Authentication required', '403');
       }
       let pendingStudiesNumber = 0;
-      for (let value of Object.values(models.consent)) {
+      let value: any;
+      for (value of Object.values(models.consent)) {
         if (value['userId'] === userId && value['consentState'] === null) {
           pendingStudiesNumber++;
         }
       }
       return pendingStudiesNumber;
     },
-    getPendingStudies: (parent, { userId }, { models }) => {
+    getPendingStudies: (parent: any, { userId }: { userId: string }, { models }: { models: any }) => {
       const pendingReqs = Object.values(models.consent).filter(
-        (req) => req.userId === userId && req.consentState === null
+        (req: any) => req.userId === userId && req.consentState === null
       );
-      return pendingReqs.map((req) => {
+      return pendingReqs.map((req: any) => {
         const studyId = req.studyId;
         const study = models.study[studyId];
         return study;
       });
     },
-    getAnsweredStudies: (parent, { userId }, { models }) => {
+    getAnsweredStudies: (parent: any, { userId }: { userId: string }, { models }: { models: any }) => {
       const answeredReqs = Object.values(models.consent).filter(
-        (req) => req.userId === userId && req.consentState !== null
+        (req: any) => req.userId === userId && req.consentState !== null
       );
-      return answeredReqs.map((req) => {
+      return answeredReqs.map((req: any) => {
         const studyId = req.studyId;
         const study = models.study[studyId];
         const studyPreference = {
@@ -53,7 +62,11 @@ const resolvers = {
   },
 
   Mutation: {
-    updateBaselinePreference: (parent, { userId, institutionType, consentState }, { models }) => {
+    updateBaselinePreference: (
+      parent: any,
+      { userId, institutionType, consentState }: { userId: string; institutionType: string; consentState: string },
+      { models }: { models: any }
+    ) => {
       models.baselinePreference[userId] = models.baselinePreference[userId] || {};
       if (typeof models.baselinePreference[userId]['userId'] === 'undefined') {
         models.baselinePreference[userId]['userId'] = userId;
@@ -65,10 +78,22 @@ const resolvers = {
         consentState,
       };
     },
-    updateStudyPreference: (parent, { userId, studyId, consentInfo }, { models }) => {
-      const reqs = Object.values(models.consent).filter((req) => req.userId === userId && req.studyId === studyId);
+    updateStudyPreference: (
+      parent: any,
+      {
+        userId,
+        studyId,
+        consentInfo,
+      }: {
+        userId: string;
+        studyId: string;
+        consentInfo: { consentState: string; declineReason: string; declineReasonOther: string };
+      },
+      { models }: { models: any }
+    ) => {
+      const reqs = Object.values(models.consent).filter((req: any) => req.userId === userId && req.studyId === studyId);
       if (reqs.length === 0) return null;
-      const req = reqs[0];
+      const req: any = reqs[0];
       req.consentState = consentInfo.consentState;
       req.declineReason = consentInfo.declineReason;
       req.declineReasonOther = consentInfo.declineReasonOther;
