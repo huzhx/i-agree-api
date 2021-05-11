8518fbe2cc690992d2125721f6747421a1b63b09
Zhaoxian Hu <huzhx@uci.edu>
2021-05-10 18:02:58 -0700

    add user-entities

---
+++ src/entities/user.ts
@@ -0,0 +1,31 @@
+import { ApolloError } from 'apollo-server-errors';
+import { UserInterface } from '../interfaces/user-interface';
+
+export class User implements UserInterface {
+  private _id: string | undefined = undefined;
+  private _isAuthed: boolean = false;
+
+  private constructor() {}
+
+  public static creator({ userId, isAuthed }: { userId: string | undefined; isAuthed: boolean }): User {
+    const user = new User();
+    user._id = userId;
+    user._isAuthed = isAuthed;
+    user.valid();
+    return user;
+  }
+
+  private valid(): void {
+    if (this._isAuthed === false || this._id === undefined) {
+      throw new ApolloError('Authentication required', '403');
+    }
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get isAuthed() {
+    return this._isAuthed;
+  }
+}
