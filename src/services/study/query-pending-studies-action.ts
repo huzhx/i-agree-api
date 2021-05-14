4de8b57be1562d9c6903eaee8d1e0fe1a0522a8a
Zhaoxian Hu <huzhx@uci.edu>
2021-05-13 23:57:53 -0700

    add application service to get the pending studies

---
+++ src/services/study/query-pending-studies-action.ts
@@ -0,0 +1,14 @@
+import { PendingStudiesRepositoryInterface } from '../../interfaces/study/pending-studies-repository-interface';
+import { StudyInterface } from '../../interfaces/study/study-interface';
+
+export class QueryPendingStudiesAction {
+  private queryPendingStudiesRepository: PendingStudiesRepositoryInterface;
+
+  constructor(queryPendingStudiesRepository: PendingStudiesRepositoryInterface) {
+    this.queryPendingStudiesRepository = queryPendingStudiesRepository;
+  }
+
+  public execute(userId: string): StudyInterface[] {
+    return this.queryPendingStudiesRepository.getBy(userId);
+  }
+}
