b7dfaca2be68833c4a63ff1177ff05c800efd16f
Zhaoxian Hu <huzhx@uci.edu>
2021-05-13 17:25:51 -0700

    add institution-type-interface

---
+++ src/interfaces/institution/institution-type-interface.ts
@@ -0,0 +1,13 @@
+enum InstitutionType {
+  DOCTORSOFFICE = 'DOCTORSOFFICE',
+  HOSPITAL = 'HOSPITAL',
+  INSURANCE = 'INSURANCE',
+  STATEHA = 'STATEHA',
+  GOVHA = 'GOVHA',
+  BIOTECHCOMPANY = 'BIOTECHCOMPANY',
+  UNIVERSITY = 'UNIVERSITY',
+}
+
+export interface InstitutionTypeInterface {
+  institutionType: InstitutionType;
+}
