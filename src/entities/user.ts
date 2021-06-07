880911538dad38833dc9db0cd82018d1d3c8caef
Zhaoxian Hu <huzhx@uci.edu>
2021-06-07 02:35:04 -0700

    update user entity

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,16 +1,18 @@
 import { UserInterface } from '../interfaces/user-interface';
-import { AuthTokenInterface } from '../interfaces/auth-token-interface';
+import { TokenDecodedInterface } from '../interfaces/token-decoded-interface';
 
 export class User implements UserInterface {
   private _id: string | undefined = undefined;
   private _email: string | undefined = undefined;
+  private _authToken: string | undefined = undefined;
 
   private constructor() {}
 
-  public static create({ userID, userEmail }: AuthTokenInterface): User {
+  public static create({ userID, userEmail, authToken }: TokenDecodedInterface): User {
     const user = new User();
     user._id = userID;
     user._email = userEmail;
+    user._authToken = authToken;
     return user;
   }
 
@@ -20,5 +22,9 @@ export class User implements UserInterface {
 
   get email() {
     return this._email;
+  }
+
+  get authToken() {
+    return this._authToken;
   }
 }
